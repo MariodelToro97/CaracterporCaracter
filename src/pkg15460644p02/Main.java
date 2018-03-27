225d4ae2a1ddff4f4ebc7495cc70da55ffa6eebc
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2018-03-27 12:38:39 -0600

    Initial commit

---
+++ src/pkg15460644p02/Main.java
@@ -0,0 +1,26 @@
+/*
+* Este programa fue hecho por Mario Josue del Toro Morales
+* con Número de Control 15460644
+* Estudiante del sexto semestre de la carrera de Ingeniería en Sistemas Computacionales
+* del Instituto Tecnológico de Colima
+* Materia de Lenguajes y Autómatas I
+******Descripción************
+* Programa que obtiene caracter por caracter de cierta cadena o texto
+ */
+package pkg15460644p02;
+
+/**
+ *
+ * @author Mario Josue del Toro Morales
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal principal = new Principal(); // Declaración de la clase que tiene el formato gráfico
+        principal.setVisible(true); //Despliegue de la ventana gráfica
+    }
+    
+}
